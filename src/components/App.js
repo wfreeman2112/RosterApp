fb45c97b1926fb4396f53274b7f24a123b698c12
Wendi Freeman <wfreeman2112@gmail.com>
2021-02-06 11:03:28 -0600

    add Welcome message

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import '../App.css';
+import Welcome from './Welcome'
+
+function App() {
+  return (
+    <div className="container">
+      <Welcome />
+     </div>
+  );
+}
+
+export default App;
---
+++ src/components/Welcome.js
@@ -0,0 +1,5 @@
+function Welcome(){
+    return <h2>Welcome Astros fans!</h2>
+}
+
+export default Welcome;
